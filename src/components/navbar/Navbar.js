cb58a93fdd4e97687b8af7a8e6885625232376ed
niloofar-palvanehcls <niloofarpalvaneh@gmail.com>
2023-11-03 11:44:42 +0330

    create mobile menu

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,15 +1,20 @@
 import Button from "../button/Button";
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Navbar() {
+    const [isShowMobileMenu, setIsShowMobileMenu] = useState(false)
+    const isShowMobileMenuHandler = () => {
+        setIsShowMobileMenu(!isShowMobileMenu)
+    }
     return (
         <>
-            <div className="flex items-center justify-between">
-                <div>
+            <div className="flex items-center justify-between px-4 py-2">
+                <div className="z-50">
                     <Image src={"/logo.png"} width={60} height={60} alt="logo" />
                 </div>
-                <div>
+                <div className="sm:hidden">
                     <ul className="flex items-center gap-12 text-[18px]">
                         <Link href={"#"} className="hover:text-fuchsia-400 transition">
                             Home
@@ -28,7 +33,33 @@ export default function Navbar() {
                         </li>
                     </ul>
                 </div>
-
+                <div className="hidden sm:flex flex-col gap-2 z-50"
+                    onClick={isShowMobileMenuHandler}
+                >
+                    <div className={`bg-white rounded w-[30px] h-[3px] transition-all ease-out duration-300 ${isShowMobileMenu ? "rotate-45" : "rotate-0"}`}></div>
+                    <div className={`bg-white rounded w-[25px] h-[3px] transition-all ease-out duration-300 ${isShowMobileMenu ? "hidden" : "inline"}`}></div>
+                    <div className={`bg-white rounded w-[30px] h-[3px] transition-all ease-out duration-300 ${isShowMobileMenu ? "-rotate-45" : "rotate-0"}`}></div>
+                </div>
+            </div>
+            <div className={`${isShowMobileMenu ? "-right-0" : "right-[100%]"} transition-all ease-out duration-300
+             absolute h-screen w-full bg-[#18153c] flex items-center justify-center`}>
+                <ul className=" flex flex-col gap-6 items-center text-[18px]">
+                    <Link href={"#"} className="hover:text-fuchsia-400 transition">
+                        Home
+                    </Link>
+                    <Link href={"#"} className="hover:text-fuchsia-400 transition">
+                        Contact
+                    </Link>
+                    <Link href={"#"} className="hover:text-fuchsia-400 transition">
+                        Product
+                    </Link>
+                    <Link href={"#"} className="hover:text-fuchsia-400 transition">
+                        Feature
+                    </Link>
+                    <li>
+                        <Button title="Build New" />
+                    </li>
+                </ul>
             </div>
         </>
     )
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <>
-      <div className='px-12 py-6 '>
+      <div className=''>
         <Navbar />
       </div>
     </>
